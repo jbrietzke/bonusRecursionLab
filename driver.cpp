2001b491001723f4b9a9e34d9c26bae0c11d08ed
Jackson Brietzke <jack.brietzke@gmail.com>
2016-11-07 20:35:33 -0500

    submittion for bonus

--- driver.cpp
+++ driver.cpp
@@ -4,14 +4,19 @@
 using namespace std;
 int main()
 {
-   Maze blah;
+   Maze myMaze;
    int row, col;
-   char blank;
-   blah.createBoard("board.txt");
-   blah.display();
+   char blank, haveFile;
+   string fileName;
+   cout << "Do you have a Maze file to import?(y/n): ";
+   cin >> haveFile;
+   if (haveFile == 'y') cin >> fileName;
+   else fileName = "board.txt";
+   myMaze.createBoard(fileName);
+   myMaze.display();
    cout << "Please enter your starting position, zero indexed(r,c): ";
    cin >> row >> blank >>col;
-   cout << "It is " << boolalpha << blah.isPath(row,col) <<
+   cout << "It is " << boolalpha << myMaze.isPath(row,col) <<
    " that you can reach the exit" << endl;
    return 0;
 }
